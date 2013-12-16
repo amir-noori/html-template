9715bf4013ddf0722bb6e3605fe4e29591cfed13
Amir Noori <ee.amir.noori@gmail.com>
2013-12-16 13:58:40 +0330

    added orange

--- second/index.js
+++ second/index.js
@@ -60,6 +60,22 @@ function tabClicked()
 	
 }
 
+var html = $('#registerForm').html();
+function register(val)
+{
+	if(val == 'back')
+		$('#registerForm').html(html);
+	else
+	{
+		$('#registerForm').html('<div class="waitImg"><img src="./wait.gif"></div>');
+		setTimeout(function(){
+			$('#registerForm').html('<div class="waitImg">You Are Registerd<br><br><a onclick="register(\'back\')">Go Back</a></div>');
+			
+		} , 2000);
+	}
+}
+
+
 function init()
 {
     var tabs = document.getElementsByClassName('tab');
@@ -90,5 +106,122 @@ function init()
 		}, 300);
 		
 	});
+	
+	
+	var topRightMenuDataBox = $('.topRightMenuDataBox');
+	var topSideMessageWrapper = $('.topSideMessageWrapper');
+	if(topSideMessageWrapper.length > 0)
+	{
+		if(topSideMessageWrapper.length == 1)
+			topRightMenuDataBox.css('margin-top' , '-170px');
+		else
+			topRightMenuDataBox.css('margin-top' , -(170 + (topSideMessageWrapper.length-1)*115) + 'px');
+	}
+	else
+		topRightMenuDataBox.css('margin-top' , '-88px');
+	
+	$('.topRightIcon').click(function(e){
+		$('.topRightMenuDataBox').fadeOut();
+		var data = '#' + e.target.id + 'Data';
+		data = $(data);
+		if(data.css('display') == 'block')
+			data.fadeOut();
+		else	
+			data.fadeIn();
+			
+		$(document).click(function(e){
+			if($(e.target).attr('class') != 'topRightIconImg')
+			$('.topRightMenuDataBox').fadeOut();	
+		});	
+	});
+	
+	$('.topMessageClose').click(function(){
+		if($(this).closest('.topSideMessageWrapper').attr('class') == 'topSideMessageWrapper , firstTopSideMessageWrapper')
+			$(this).closest('.topSideMessageWrapper').next('.topSideMessageWrapper').attr('class' , 'topSideMessageWrapper , firstTopSideMessageWrapper')
+		$(this).closest('.topSideMessageWrapper').fadeOut(function(){
+			$(this).closest('.topSideMessageWrapper').remove();
+			
+		});
+		
+		setTimeout(function(){
+			setTimeout(function(){
+				var topRightMenuDataBox = $('.topRightMenuDataBox');
+				var topSideMessageWrapper = $('.topSideMessageWrapper');
+				if(topSideMessageWrapper.length > 0)
+				{
+					if(topSideMessageWrapper.length == 1)
+						topRightMenuDataBox.css('margin-top' , '-170px');
+					else
+						topRightMenuDataBox.css('margin-top' , -(170 + (topSideMessageWrapper.length-1)*115) + 'px');
+				}
+				else
+					topRightMenuDataBox.css('margin-top' , '-88px');
+					
+			} , 200);
+			
+		} , 300)
+	});
+	
+	
+	$('.editInfoBtn').click(function(){
+		var editInfoBtn = $(this);
+		$(this).parent().parent().parent().parent('.editInfoHeader').next('.editInfoContent').toggle('slow' , function(){
+			if(editInfoBtn.text() == 'EDIT')
+				editInfoBtn.text('CLOSE'); 
+			else
+				editInfoBtn.text('EDIT');
+		});
+	});
+	
+	$('.topFormClose img').click(function(){
+		$('#topFormContainer').fadeOut();
+		$('#parentContainer').attr('class' , '');
+	});	
+	
+	
+	/*
+		top form close icon
+	*/
+	$('.topMessageClose').click(function(){	
+		$(this).closest('.topFormMessageWrapper').next('.topFormMessageWrapper').attr('class' , 'topFormMessageWrapper')
+		$(this).closest('.topFormMessageWrapper').fadeOut(function(){
+			$(this).closest('.topFormMessageWrapper').remove();
+		});
+	});
+	/*******/
+	
+	
+	/*
+		show overlay form
+	*/
+	$('.showOverLayFrom').click(function(){	
+		$('#topFormContainer').fadeIn();
+		$('#parentContainer').attr('class' , 'parentContainerDisable');
+	});
+	/*******/
+	
+	/*
+		accordionInfoTable
+	*/
+	$('.accordionInfoTableExpand').click(function(){	
+		var accordionInfoTableContent = $(this).parent().next('tr').children('.accordionInfoTableContent');
+		var THIS = $(this);
+		if(accordionInfoTableContent.css('display') == 'none'){
+			accordionInfoTableContent.fadeIn();
+			THIS.html('-');
+			THIS.css('font-size' , '60px');
+		}
+		else{
+			accordionInfoTableContent.fadeOut();
+			THIS.html('+');
+			THIS.css('font-size' , '42px');
+		}
+	});
+	/*******/
 }
 document.onload = init();
+
+
+
+
+
